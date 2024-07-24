fb6f20ace135e4b8ab9bbf7506a7b94dc2a75741
Anthony Cossins <1451668+anthonyec@users.noreply.github.com>
2024-07-24 10:23:55 +0100

    Get it working with framer

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import './reset.css'
 import App from './app.svelte'
+import { framer } from "framer-plugin"
+
+void framer.showUI({
+  title: "Find Things",
+  position: "top right",
+  width: 320,
+  height: 420,
+})
 
 const app = new App({
   target: document.getElementById('app')!,
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import mkcert from "vite-plugin-mkcert"
+import framer from "vite-plugin-framer"
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), mkcert(), framer()],
 })
